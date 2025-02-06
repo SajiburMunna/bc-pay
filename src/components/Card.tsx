ae749f2d0afdd02a359a3daeb918bebbf89b3420
sajiburmunna <sajiburmunna087@gmail.com>
2025-02-06 22:14:33 +0600

    chore: initial project setup

---
+++ src/components/Card.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Card() {
+  return (
+    <div className="">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis
+      perspiciatis reprehenderit fuga, esse sapiente fugit veniam possimus vel
+      quasi eum, quae doloribus! Totam mollitia perferendis nulla illum facere
+      nostrum autem!
+    </div>
+  );
+}
+
+export default Card;
