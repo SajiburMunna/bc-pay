82b26e3f9fe349162bd2f35764470c60b6656dae
sajiburmunna <sajiburmunna087@gmail.com>
2025-02-07 15:28:34 +0600

    feat(dashboard): profile and logout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,51 @@
-import React from "react";
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useLayoutEffect, useState } from "react";
+
+import CookieService from "@/services/CookieService";
+import DropdownMenu from "./DropdownMenu";
 
 function Header() {
+  const router = useRouter();
+  const [role, setRole] = useState("");
+
+  useLayoutEffect(() => {
+    const crole = CookieService.get("role");
+    if (crole) {
+      setRole(crole);
+    }
+  }, []);
+  const handleLogout = () => {
+    CookieService.remove("role");
+    router.push("/login");
+  };
   return (
     <header className="flex items-center justify-between px-6 py-4 bg-white">
-      <div>Logo</div>
-      <div>User</div>
+      <p className="font-bold text-lg text-primary">BCPay</p>
+      <div>
+        <DropdownMenu
+          button={
+            <button className="flex justify-center items-center text-white bg-primary h-10 w-10 px-4 py-4 rounded-full font-bold">
+              {role?.charAt(0)?.toUpperCase()}
+            </button>
+          }
+          optionClassName="min-w-[200px]"
+        >
+          <span>
+            Welcome
+            <span className="capitalize font-bold text-primary ml-1">
+              {role?.toLocaleUpperCase()}
+            </span>
+          </span>
+          <div
+            onClick={handleLogout}
+            className="bg-slate-200 text-red-500 p-2 rounded-lg mt-2 cursor-pointer"
+          >
+            Logout
+          </div>
+        </DropdownMenu>
+      </div>
     </header>
   );
 }
